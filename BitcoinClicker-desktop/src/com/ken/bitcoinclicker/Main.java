3963dfe7fb833c804d6cc326c65f95d76452f6f4
SirKenson <kennethb1296@gmail.com>
2014-03-04 14:57:15 -0700

    Initial Push
    Initial push involves only screen size

---
+++ BitcoinClicker-desktop/src/com/ken/bitcoinclicker/Main.java
@@ -0,0 +1,16 @@
+package com.ken.bitcoinclicker;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+
+public class Main {
+	public static void main(String[] args) {
+		LwjglApplicationConfiguration cfg = new LwjglApplicationConfiguration();
+		cfg.title = "BitcoinClicker";
+		cfg.useGL20 = false;
+		cfg.width = 600;
+		cfg.height = 420;
+		
+		new LwjglApplication(new BitcoinClickerGame(), cfg);
+	}
+}
